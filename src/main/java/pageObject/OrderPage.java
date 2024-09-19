f81e0547c29b0c9746a5fc357e413379003f5d7f
Жеконя <chernyshevqa@gmail.com>
2024-09-19 20:24:57 +0300

    fix mistakes in TestIsVisibleTextOnMainPage,OrderPage, TestScooterOrder classes

--- src/main/java/pageObject/OrderPage.java
+++ src/main/java/pageObject/OrderPage.java
@@ -10,58 +10,43 @@ import java.util.List;
 
 public class OrderPage {
     private WebDriver driver;
-
     //поле имя
     private By nameField = By.xpath("//input[@placeholder='* Имя']");
-
     // поле фамилия
     private By surnameField = By.xpath("//input[@placeholder='* Фамилия']");
-
     // поле адрес
     private By addressField = By.xpath("//input[@placeholder='* Адрес: куда привезти заказ']");
-
     // выпадающий список метро
     private By metroList = By.xpath("//input[@placeholder='* Станция метро']");
-
     // поле телефон
     private By phoneField = By.xpath("//input[@placeholder='* Телефон: на него позвонит курьер']");
-
     // кнопка далее
     private By nextButton = By.xpath("//button[contains(text(),'Далее')]");
-
     // календарь
     private By calendar = By.xpath("//input[@placeholder='* Когда привезти самокат']");
-
     // выпадающий список срок аренды
     private By rentList = By.xpath("//div[@class='Dropdown-placeholder']");
-
     // чекбокс выбора цвета черный
     private By blackCheckBox = By.xpath("//input[@id='black']");
-
     // чекбокс выбора цвета серый
     private By greyCheckBox = By.xpath("//input[@id='grey']");
-
     // комментарий для курьера
     private By commentForDeliver = By.xpath("//input[@placeholder='Комментарий для курьера']");
-
     //кнопка заказать
     private By orderButton = By.xpath("//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
-
     //кнопка подтверждения заказа
     private By confirmButton = By.xpath("//button[contains(text(),'Да')]");
-
+    //верхняя кнопка заказа
     private By topOrderButton = By.xpath("//button[@class='Button_Button__ra12g']");
     //кнопка заказать снизу
     private By middleOrderButton = By.xpath("//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
-
+    //кнопка принятия кукисов
     private By conferCookieButton = By.xpath("//button[@id='rcc-confirm-button']");
-
+    //кнопка "Да" при заказе
     private By buttonYes = By.xpath("//button[contains(text(),'Да')]");
-
-    private By orderText = By.xpath("//div[@class='Order_ModalHeader__3FDaJ']");
-
+    //локатор текста на странице
     private By textOnMainPage = By.xpath("//div[@class='Home_Header__iJKdX']//div[1]");
-
+    //кнопка главной страницы заказа скутера
     private By ButtonScooter = By.xpath("//img[@alt='Scooter']");
 
     public OrderPage(WebDriver driver) {
@@ -74,7 +59,7 @@ public class OrderPage {
                           String orderDate, String colour, String comment, String buttonType) {
         //принимаем куки
         clickElement(conferCookieButton);
-        //нажимаем на верхнюю кнопку заказать
+        //нажимаем на заказать
         if (buttonType.equals("top")) {
             clickElement(topOrderButton);
         } else if (buttonType.equals("middle")) {
